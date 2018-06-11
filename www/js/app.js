afcc8c72de3d476b693b12aca55a5007303382ee
Keith Gudger <kgudger@gmail.com>
2018-06-11 13:50:34 -0700

    Added real side offsets, cleaned up code, added current postiion

--- www/js/app.js
+++ www/js/app.js
@@ -7,18 +7,80 @@ Neither the name of the nor the names of its contributors may be used to endorse
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-var currentLatitude = 36.9741;
-var currentLongitude = -122.0308
+var currentLatitude = 0;
+var currentLongitude = 0;
+var options = {			// GPS options
+    timeout: 5000,
+    maximumAge: 20000,
+    enableHighAccuracy: true
+};
 var geocoder;
-var mapCanvas = document.getElementById('map_canvas');
+var mapCanvas;
 var _map ;
+var s1side = 0 ;
+var debug = true;
 
-console.log("In app.js");
+function consoLog(message) {
+	if (debug) 
+		console.log(message);
+}
+
+consoLog("In app.js");
 
     function initialize_map() {
-		console.log("In initalize");
-//		directionsService = new google.maps.DirectionsService();
-		latlngp = new google.maps.LatLng(currentLatitude, currentLongitude); // using global variable now
+		consoLog("In initalize");
+		mapCanvas = document.getElementById('map_canvas');
+		ready();
+	};
+
+/**
+ *	Fires when DOM page loaded
+ */
+function ready() {
+    if (navigator.geolocation) {
+		var location_timeout = setTimeout("defaultPosition()", 2000);
+		// changed to 2 seconds 
+        navigator.geolocation.getCurrentPosition(
+			function(pos) { clearTimeout(location_timeout); showPosition(pos); },
+			function(err) {
+				clearTimeout(location_timeout);
+				console.warn('ERROR(' + err.code + '): ' + err.message);
+				defaultPosition()
+			},
+			options
+		);
+    }
+    else {
+		console.warn("Geolocation failed. \nPlease enable GPS in Settings.", 1);
+		defaultPosition();
+	}
+}
+
+/** 
+ *	sets current latitude and longitude from ready() function
+ */
+function showPosition(position) {
+    currentLatitude = position.coords.latitude;
+	currentLongitude = position.coords.longitude;
+    consoLog("Lat is " + currentLatitude + " Lon is " + currentLongitude);
+    showMap();
+};
+
+/** 
+ *	on fail from ready, default position
+ */
+function defaultPosition() {
+	currentLatitude = 36.9741;
+	currentLongitude = -122.0308
+    consoLog("In defaultPosition Lat is " + currentLatitude + " Lon is " + currentLongitude);
+    showMap();
+}
+
+/**
+ *	showMap shows map after geolocation decided.
+ */
+function showMap() {
+		var latlngp = new google.maps.LatLng(currentLatitude, currentLongitude); // using global variable now
 	    var mapOptions = {
 			center: latlngp,
 			zoomControl: true,
@@ -30,73 +92,14 @@ console.log("In app.js");
     	    mapTypeId: google.maps.MapTypeId.roadmap
     	};
         _map = new google.maps.Map(mapCanvas, mapOptions);
-//		setTimeout("$('#map_canvas').gmap('refresh')",500);
-/*		directionsDisplay = new google.maps.DirectionsRenderer();
-		directionsDisplay.setMap(_map);
-		directionsDisplay.setPanel(document.getElementById('natsinglelist'));*/
 		var marker = new google.maps.Marker({
 			position: latlngp,
 			map: _map,
 			title: 'Current Position'
 		});
 		marker.setMap(_map);
+} // showMap
 
-	};
-
-/**	buildField
- *	builds field from lat lon
- *
- *	@param latitude is farthest latitude from center
- *	@param longitude is farthest longitude from center
- *	@param color is color of polygon
- *	@param cname is name of field
- *	@param crop is crop type
- */
-	function buildField(latitude, longitude, color, cname, crop) {
-		var fieldCoords = [
-			new google.maps.LatLng(36.6502227, -121.7946188),
-			new google.maps.LatLng(latitude, -121.7946188),
-			new google.maps.LatLng(latitude, longitude),
-			new google.maps.LatLng(36.6502227, longitude),
-			new google.maps.LatLng(36.6502227, -121.7946188)
-		];
-
-  // Construct the polygon.
-  		var cropField = new google.maps.Polygon({
-		    paths: fieldCoords,
-		   	strokeColor: color,
-		    strokeOpacity: 0.8,
-		    strokeWeight: 2,
-		    fillColor: color,
-		    fillOpacity: 0.35,
-		    draggable: true,
-		    editable: true,
-		    geodesic: true
-		});
-
-		cropField.setMap(_map);
-
-		var latavg = ((latitude - 36.650227) / 2) + 36.650227 ;
-		var lonavg = ((longitude - (-121.7946188)) / 2) - 121.7946188 ;
-		var myLatlng = new google.maps.LatLng(latavg,lonavg);
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: _map,
-			title: 'Current Position'
-		});
-		marker.setMap(_map);
-	    var infoWindow = new google.maps.InfoWindow();
-		var pname = '<div style="color: black;">' + "Field " + cname + "<br>" + 
-			'date planted: 24 Jan 15<br>' +
-			'stock company: MV Seeds<br>' +  
-			'lot number: 06ncai01<br>' +
-			'plant type: ' + crop +'</div>';
-		google.maps.event.addListener(marker, 'click', function() {
-  			console.log('Vertex moved on outer path.');
-	        infoWindow.setContent(pname);
-        	infoWindow.open(_map, marker);
-		});
-	};
 
 	function handleNoGeolocation(errorFlag) {
 		if (errorFlag) {
@@ -120,7 +123,7 @@ console.log("In app.js");
 	 *	@param p is passed from intel library function
 	 */
 	var suc = function(p) {
-//	    console.log("geolocation success", 4);
+//	    consoLog("geolocation success", 4);
 	    //Draws the map initially
     	currentLatitude = p.coords.latitude;
     	currentLongitude = p.coords.longitude;
@@ -130,17 +133,13 @@ console.log("In app.js");
 	 *	fail function for intel gps routine - does nothing 
 	 */			    
 	var fail = function() {
-	    console.log("Geolocation failed. \nPlease enable GPS in Settings.", 1);
+	    consoLog("Geolocation failed. \nPlease enable GPS in Settings.", 1);
 		document.getElementById("geostat").innerHTML="Geolocation failed. \nPlease enable GPS in Settings.";
 	};
 
 /**	updateMap
  *	
- *	@param latitude is farthest latitude from center
- *	@param longitude is farthest longitude from center
- *	@param color is color of polygon
- *	@param cname is name of field
- *	@param crop is crop type
+ *	@param features is from AJAX call
  */
 	function updateMap(features) {
 	  document.getElementById("add_place").innerHTML=features['Address'];
@@ -152,7 +151,7 @@ console.log("In app.js");
        geocoder.geocode( { 'address': features['Address']}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
           if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-			console.log("Geocode OK" + results[0].geometry.location);
+			consoLog("Geocode OK" + results[0].geometry.location);
           _map.setCenter(results[0].geometry.location);
 
             var infowindow = new google.maps.InfoWindow(
@@ -170,7 +169,7 @@ console.log("In app.js");
             });
             var apnnd = features['APN']
             apnnd = apnnd.replace(/-/g, '')
- 			console.log("APN " + apnnd);
+ 			consoLog("APN " + apnnd);
             getPoly(apnnd)
 
           } else {
@@ -188,18 +187,14 @@ var reducedPoly ;
 
 /**	updatePoly
  *	
- *	@param latitude is farthest latitude from center
- *	@param longitude is farthest longitude from center
- *	@param color is color of polygon
- *	@param cname is name of field
- *	@param crop is crop type
+ *	@param features from AJAX call
  */
 	function updatePoly(features) {
 		var fieldCoords = [];
 		var arrayLength = features.length;
 		var property1 = [] ;
 		for (var i = 0; i < arrayLength; i++) {
-			console.log("1=" + features[i][1] + " 0= " + features[i][0]);
+			consoLog("1=" + features[i][1] + " 0= " + features[i][0]);
 			fieldCoords.push(new google.maps.LatLng(features[i][1], features[i][0]));
 			property1.push(new Vector2(features[i][1], features[i][0]));
 		}
@@ -221,10 +216,10 @@ var reducedPoly ;
 //		New smaller polygon goes here
 		fieldCoords = [];
 		if (typeof reducedPoly !== 'undefined') { reducedPoly.setMap(null); }
-		var fieldCoords2 = inflatePolygon(property1, -2.74319999583e-5); // 10 feet
+		var fieldCoords2 = inflatePolygon(property1, s1side * -2.74319999583e-6); // 10 feet
 		arrayLength = fieldCoords2.length;
 		for (var i = 0; i < arrayLength; i++) {
-			console.log("X =" + fieldCoords2[i].x + " Y = " + fieldCoords2[i].y);
+			consoLog("X =" + fieldCoords2[i].x + " Y = " + fieldCoords2[i].y);
 			fieldCoords.push(new google.maps.LatLng(fieldCoords2[i].x.toString(),
 				fieldCoords2[i].y.toString()));
 		}
@@ -244,38 +239,45 @@ var reducedPoly ;
 
   }		
 
+/** vectorCoordinates2JTS 
+ * @param polygon is polygon to push coordinates into object
+ */
 function vectorCoordinates2JTS (polygon) {
   var coordinates = [];
 
   for (var i = 0; i < polygon.length; i++) {
     coordinates.push(new jsts.geom.Coordinate(polygon[i].x, polygon[i].y));
-    console.log("X = " + coordinates[i].x + " and Y = " + coordinates[i].y);
+    consoLog("X = " + coordinates[i].x + " and Y = " + coordinates[i].y);
   }
   return coordinates;
 }
 
+/** inflatePolygon
+ * @param poly
+ * @param spacing
+ */
 function inflatePolygon(poly, spacing)
 {
   var geoInput = vectorCoordinates2JTS(poly);
   geoInput.push(geoInput[0]);
-  console.log("geoInput " + geoInput[0].x);
+  consoLog("geoInput " + geoInput[0].x);
 
   var geometryFactory = new jsts.geom.GeometryFactory();
 
   try {
 	var shell = geometryFactory.createPolygon(geoInput);
-    console.log("shell is " + shell);
+    consoLog("shell is " + shell);
   }
   catch(err) {
-    console.log("Error is " + err.message);
+    consoLog("Error is " + err.message);
   }
 
   try {
     var polygon = shell.buffer(spacing, jsts.operation.buffer.BufferParameters.CAP_FLAT);
-    console.log("poly is " + polygon);
+    consoLog("poly is " + polygon);
   }
   catch(err) {
-    console.log("Error is " + err.message);
+    consoLog("Error is " + err.message);
   }
 
   var inflatedCoordinates = [];
@@ -285,25 +287,16 @@ function inflatePolygon(poly, spacing)
     var oItem;
     oItem = oCoordinates[i];
     inflatedCoordinates.push(new google.maps.LatLng(oItem.x, oItem.y));
-    console.log("iX,Y = " + oItem.x + " and iY = " + oItem.y);
+    consoLog("iX,Y = " + oItem.x + " and iY = " + oItem.y);
   }
   return oCoordinates ; //inflatedCoordinates;
 }
-/*
-var property1 = [
-	new Vector2(36.9530434040848,   -122.03296429697015),
-	new Vector2(36.95296468651526,  -122.03368781492003),
-	new Vector2(36.953773541799656, -122.03382895603836),
-	new Vector2(36.953826315585516, -122.03329982113146),
-	new Vector2(36.95362253861876,  -122.03326626425898),
-	new Vector2(36.95363807256827,  -122.03311978038441),
-	new Vector2(36.95357078962553,  -122.03310783112806),
-	new Vector2(36.953581522214364, -122.03301982319464),
-	new Vector2(36.95340744275806,  -122.03298593217909),
-	new Vector2(36.953403543286235, -122.03301802809266),
-	new Vector2(36.953125713309255, -122.0329765775434),
-	new Vector2(36.9530434040848,   -122.03296429697015)
-];*/
+
+/** Vector2
+ *  @param x is x
+ *  @param y is y
+ *  @return object with .x and .y
+ */
 function Vector2(x, y) 
 {
     this.x = x;
@@ -311,7 +304,7 @@ function Vector2(x, y)
 }
 
 
-/**
+/** sendfunc
  *	"Ajax" function that sends and processes xmlhttp request
  *	@param params is GET request string
  */
@@ -320,12 +313,12 @@ function sendfunc() {
 	var address = document.getElementById("address").value
 //	address += " Santa Cruz CA";
 	address = encodeURI(address);
-	console.log("Address is " + address);
+	consoLog("Address is " + address);
 	try {
 	   xmlhttp=new XMLHttpRequest();
     } catch(e) {
         xmlhttp = false;
-        console.log(e);
+        consoLog(e);
     }
 	if (xmlhttp) {
         xmlhttp.onreadystatechange=function() {
@@ -335,10 +328,10 @@ function sendfunc() {
               returnedList = (xmlhttp.responseText);
               returnedList = JSON.parse(returnedList);
               var features = returnedList['features']['0']['attributes'];
-              console.log(features);
+              consoLog(features);
               var zone = features['Zoning1'];
               zone = zone.split(" ");
-              console.log("zone is " + zone[0]);
+              consoLog("zone is " + zone[0]);
               getZones(zone[0]);
               updateMap(features);
 			}
@@ -347,23 +340,20 @@ function sendfunc() {
 	}
 	xmlhttp.open("GET","https://vw8.cityofsantacruz.com/server/rest/services/search/MapServer/0/query?f=json&where=Upper(Address)%20LIKE%20Upper(%27%25" + address + "%25%27)&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=Address%2CSiteAdd2%2CAPN%2CUseCode%2CParcelSizeSqFt%2CCOASTALZ%2CZoning1&outSR=102643", true);
 	xmlhttp.send(null);
-/*      xmlhttp.setRequestHeader ("Accept", "text/plain");
-	  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xmlhttp.send(params);*/
 }; // sendfunc
 
-/**
+/** getPoly
  *	"Ajax" function that sends and processes xmlhttp request
- *	@param params is GET request string
+ *	@param apn is APN from previous AJAX function
  */
 function getPoly(apn) {
     var xmlhttp;
-	console.log("APN is " + apn);
+	consoLog("APN is " + apn);
 	try {
 	   xmlhttp=new XMLHttpRequest();
     } catch(e) {
         xmlhttp = false;
-        console.log(e);
+        consoLog(e);
     }
 	if (xmlhttp) {
         xmlhttp.onreadystatechange=function() {
@@ -373,11 +363,7 @@ function getPoly(apn) {
               returnedList = (xmlhttp.responseText);
               returnedList = JSON.parse(returnedList);
               var features = returnedList['features']['0']['geometry']['rings']['0'];
-              console.log(features);
-/*              var zone = features['Zoning1'];
-              zone = zone.split(" ");
-              console.log("zone is " + zone[0]);
-              getZones(zone[0]);*/
+              consoLog(features);
               updatePoly(features);
 			}
 		  }
@@ -385,23 +371,20 @@ function getPoly(apn) {
 	}
 	xmlhttp.open("GET","https://gis.co.santa-cruz.ca.us/sccgis/rest/services/OpenData_Build_Single/MapServer/145/query?where=APNNODASH%20%3D%20%27" + apn + "%27&outFields=GP_LANDUSE&outSR=4326&f=json", true);
 	xmlhttp.send(null);
-/*      xmlhttp.setRequestHeader ("Accept", "text/plain");
-	  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xmlhttp.send(params);*/
 }; // getPoly
 
-/**
+/** getZones
  *	"Ajax" function that sends and processes xmlhttp request
- *	@param params is GET request string
+ *	@param zone is zoning designation for call to db
  */
 function getZones(zone) {
     var xmlhttp;
-	console.log("Zone is " + zone);
+	consoLog("Zone is " + zone);
 	try {
 	   xmlhttp=new XMLHttpRequest();
     } catch(e) {
         xmlhttp = false;
-        console.log(e);
+        consoLog(e);
     }
 	if (xmlhttp) {
         xmlhttp.onreadystatechange=function() {
@@ -410,10 +393,11 @@ function getZones(zone) {
             {
               returnedList = (xmlhttp.responseText);
               returnedList = JSON.parse(returnedList);
-              console.log(returnedList);
+              consoLog(returnedList);
               document.getElementById("fset_place").innerHTML=returnedList['sfront'];
               document.getElementById("rset_place").innerHTML=returnedList['srear'];
               document.getElementById("sset_place").innerHTML=returnedList['s1side'];
+              s1side = returnedList['s1side'];
 			  document.getElementById("mbuild_place").innerHTML=returnedList['maxbuild'];
 			  document.getElementById("mheight_place").innerHTML=returnedList['pheight'];
 			  document.getElementById("mstories_place").innerHTML=returnedList['pstories'];
@@ -423,7 +407,4 @@ function getZones(zone) {
 	}
 	xmlhttp.open("GET","http://feasibuild.tk/server.php?command=getZone&zone=" + zone, true);
 	xmlhttp.send(null);
-/*      xmlhttp.setRequestHeader ("Accept", "text/plain");
-	  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xmlhttp.send(params);*/
 }; // getZones
